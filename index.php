3fc09954f90047c7a89b778ae44be2d02ac395ac
paulinho.fulle <paulohfulle@gmail.com>
2022-03-31 02:28:27 -0300

    finalizando

--- index.php
+++ index.php
@@ -1,5 +1,6 @@
-<?php
+<?php 
     require_once('./processamento/process.php');
+    require_once './processamento/EnumCalculator.php';
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -11,38 +12,38 @@
     <title>Calculadora</title>
 </head>
 <body>
-    
+<?php
+    $oTeste = new process();
+    $oTeste->iniciaProcessamento();
+?>
     <form method="POST">
         <div class="div">
-            <input type="text" class="tela" value="<?= process::exibeVisor()?>" name="tela">
+            <input type="text" class="tela" value="<?= process::exibeVisor()?>" name="tela" disabled="">
             <br>
             <button type="submit" class="botao" value="1" name="valor">1</button>
             <button type="submit" class="botao" value="2" name="valor">2</button>
             <button type="submit" class="botao" value="3" name="valor">3</button>
-            <button type="submit" class="botao" value="divisao" name="operador">/</button>
+            <button type="submit" class="botao" value="/" name="operador">/</button>
 
             <br>
             <button type="submit" class="botao" value="4" name="valor">4</button>
             <button type="submit" class="botao" value="5" name="valor">5</button>
             <button type="submit" class="botao" value="6" name="valor">6</button>
-            <button type="submit" class="botao" value="multiplicacao" name="operador">*</button>
+            <button type="submit" class="botao" value="*" name="operador">*</button>
 
             <br>
             <button type="submit" class="botao" value="7" name="valor">7</button>
             <button type="submit" class="botao" value="8" name="valor">8</button>
             <button type="submit" class="botao" value="9" name="valor">9</button>
-            <button type="submit" class="botao" value="adicao" name="operador">+</button>
+            <button type="submit" class="botao" value="+" name="operador">+</button>
             
             <br>
             <button type="submit" class="botao" value="0" name="valor">0</button>
             <button type="submit" class="botao" value="limpa" name="limpa">C</button>
-            <button type="submit" class="botao" value="subtracao" name="operador">-</button>
+            <button type="submit" class="botao" value="-" name="operador">-</button>
             <button type="submit" class="botao" value="total" name="total">=</button>
         </div>
-        <?php
-            $oTeste = new process();
-            $oTeste->iniciaProcessamento();
-        ?>
+
     </form>
 </body>
 </html>
--- processamento/process.php
+++ processamento/process.php
@@ -1,4 +1,5 @@
 <?php
+require_once('EnumCalculator.php');
 
 class Process {
 
@@ -9,27 +10,79 @@ class Process {
 
     public function iniciaCalculadora(){
         session_start();
-        if (!isset($_SESSION['tela']) && !isset($_SESSION['operador']) && !isset($_SESSION['valor']))
-        {
-            $_SESSION['tela'] = '';
-            $_SESSION['operador'] = '';
-            $_SESSION['valor'] = '';
-        }
-        
     }
 
     public function realizaProcessamento(){
-        if(!empty($_GET)){
-            if($_GET['valor']){
-                
+        if(!empty($_POST)){
+            if(isset($_POST['valor'])){
+                $iValor = $_POST['valor'];
+                if(empty($_SESSION['valorAntigo'])){
+                    $_SESSION['valorAntigo'] = $iValor;
+                    $_SESSION['tela'] = $_SESSION['valorAntigo'];
+                } 
+                else if(!empty($_SESSION['valorAntigo']) && empty($_SESSION['operador'])){
+                    $_SESSION['valorAntigo'] .= $iValor;
+                    $_SESSION['tela'] = $_SESSION['valorAntigo'];
+                }
+                else if(empty($_SESSION['valorAntigo2'])){
+                    $_SESSION['valorAntigo2'] = $iValor;
+                    $_SESSION['tela'] = $_SESSION['valorAntigo2'];
+                }
+                else {
+                    $_SESSION['valorAntigo2'] .= $iValor;
+                    $_SESSION['tela'] = $_SESSION['valorAntigo2'];
+                }
             }
+            else if(isset($_POST['operador'])){
+                $xOperador = $_POST['operador'];
+                if(empty($_SESSION['operador'])){
+                    $_SESSION['operador'] = $xOperador;
+                }
+                $_SESSION['tela'] = '';
+            }
+            else if(isset($_POST['total'])) {
+                $this->realizaCalculo($_SESSION['operador'], $_SESSION['valorAntigo'], $_SESSION['valorAntigo2']);
+            }
+            else if($_POST['limpa']) {
+                $_SESSION['tela'] = '';
+                $_SESSION['valorAntigo'] = '';
+                $_SESSION['valorAntigo2'] = '';
+                $_SESSION['valor'] = '';
+                $_SESSION['operador'] = '';
+            }
+        }
+    }
+    
+    public function realizaCalculo($xOperador, $iValorA, $iValorB) {
+        switch ($xOperador) {
+            case '+':
+                $_SESSION['tela'] = $iValorA + $iValorB;
+                $_SESSION['valorAntigo'] = $iValorA + $iValorB;
+                $_SESSION['operador'] = '';
+                break;
+            case '-':
+                $_SESSION['tela'] = $iValorA - $iValorB;
+                $_SESSION['valorAntigo'] = $iValorA - $iValorB;
+                $_SESSION['operador'] = '';
+                break;
+            case '*':
+                $_SESSION['tela'] = $iValorA * $iValorB;
+                $_SESSION['valorAntigo'] = $iValorA * $iValorB;
+                $_SESSION['operador'] = '';
+                break;
+            case '/':
+                $_SESSION['tela'] = $iValorA / $iValorB;
+                $_SESSION['valorAntigo'] = $iValorA / $iValorB;
+                $_SESSION['operador'] = '';
+                break;
+            default:
+                break;
         }
     }
 
     public static function exibeVisor(){
-        return $_POST['tela'];
+        return $_SESSION['tela'];
     }
 
 }
-
 ?>
